89973da8e5cd25b47029dafb8e8c39a2eccd9bba
sabrinaachille <92241507+sabrinaachille@users.noreply.github.com>
2024-02-13 17:33:20 -0500

    Update Course Class
    I updated the course class with data members and methods.

--- src/main/java/com/mycompany/csc229_assignment01/Course.java
+++ src/main/java/com/mycompany/csc229_assignment01/Course.java
@@ -34,5 +34,24 @@ package com.mycompany.csc229_assignment01;
  * @author MoaathAlrajab
  */
 public class Course {
+ 
+    private String ID;
+    private String Name;
+    private int Code;
+    //setter
+    public void setID(String id){ this.ID = id;}
+    public void setName(String name){this.Name = name;}
+    public void setCode(int code){this.Code = code;}
+    //getters
+    public String getID(){return ID;}
+    public String getName(){return Name;}
+    public int getCode(){return Code;}
+    //constructors
+    Course(){
+        ID = "Unknown"; Name = "Unknown"; Code = 0;
+    }
+    Course(String name, String id, int code){
+        ID = id; Name = name; Code = code;
+    }
 
 }
